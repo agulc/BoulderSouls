da224bfe892266e0f9bfcfe786d82220a0f2c458
unknown <fedeblasco@hotmail.com>
2017-10-12 23:07:13 -0300

    getPos

--- Boulder Dash/src/Main/Posicion.java
+++ Boulder Dash/src/Main/Posicion.java
@@ -19,4 +19,11 @@ public class Posicion {
 	public void setY(int y){
 		this.coordY = y;
 	}
+	
+	int[] getPos(){
+		int[] posiciones = new int[2];
+		posiciones[1]=this.coordX;
+		posiciones[2]=this.coordY;
+		return posiciones;//Holis
+	}
 }
