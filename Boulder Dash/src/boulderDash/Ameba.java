7c00a09feef47ea139b74a8d2a200ae971ab2809
Nicolas <ghigliazzanicolas@gmail.com>
2017-10-22 19:57:13 -0300

    Expansion Ameba

--- Boulder Dash/src/boulderDash/Ameba.java
+++ Boulder Dash/src/boulderDash/Ameba.java
@@ -1,6 +1,9 @@
 package boulderDash;
+import java.util.Random;
 
 public class Ameba extends Enemigo{
+	
+	Random generador = new Random();
 	
 	Ameba(int x,int y){
 		super(x,y);
@@ -10,9 +13,65 @@ public class Ameba extends Enemigo{
 		return false;
 	}
 	
-	public void activarIA(){	
+	private void expandirse (int offsetX, int offsetY) throws Exception//Crea una ameba en la posicion con el offset recibido, si esta es Suciedad o Vacio
+	{
+		Posicion pos = new Posicion();
+		pos.setX(this.getPos().getX() + offsetX);
+		pos.setY(this.getPos().getY() + offsetY);
 		
-		//IMPLEMENTAR EXPLANSION/REPRODUCCION
+		if (Mapa.getInstancia().getPersonaje(pos) instanceof Suciedad || Mapa.getInstancia().getPersonaje(pos) instanceof Vacio)
+		{
+			Ameba hijo = new Ameba(pos.getX(),pos.getY());
+			Mapa.getInstancia().setPersonaje(hijo);
+		}
+	}
+	
+	public void activarIA()throws Exception{	
+		
+		int expandirse = 1 + generador.nextInt(1000); //Genera un numero del 1 al 1000
+		int adyacente = 1 + generador.nextInt(8); //Determina en que cuadro adyacente intentara expandirse
+	
+		
+		if (expandirse == 1000) //Tiene una probabilidad de 1 en 1000 de intentar expandirse
+		{
+			switch (adyacente)
+			{
+			case 1: 
+				expandirse(-1,-1);
+				break;
+				
+			case 2: 
+				expandirse(-1,1);
+				break;
+				
+			case 3: 
+				expandirse(1,1);
+				break;
+				
+			case 4: 
+				expandirse(1,-1);
+				break;
+				
+			case 5: 
+				expandirse(0,1);
+				break;
+				
+			case 6: 
+				expandirse(0,-1);
+				break;				
+				
+			case 7: 
+				expandirse(1,0);
+				break;				
+				
+			default: 
+				expandirse(-1,0);
+				break;				
+			}
+		}
+		
+		//IMPLEMENTAR EXPANSION/REPRODUCCION
+		
 		
 	}
 	
