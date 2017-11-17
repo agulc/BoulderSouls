740089998b6a595ae79cc5cf5400139e5805ade5
unknown <fedeblasco@hotmail.com>
2017-11-17 00:57:33 -0300

    Por las duda

--- Boulder Dash/src/com/boulderdash/principal/Juego.java
+++ Boulder Dash/src/com/boulderdash/principal/Juego.java
@@ -1,6 +1,5 @@
 package com.boulderdash.principal;
 
-import com.boulderdash.enumerativos.ParaDonde;
 import com.boulderdash.teclaescucha.MiTeclaEscucha;
 
 import javax.swing.*;
@@ -10,7 +9,6 @@ public class Juego {
 	 * Programa principal con los movimientos que Rockford
 	 * realizará en los niveles 1 y 2.
 	 * @param args .
-	 * @throws Exception .
 	 */
 	public static void main(String args[]){
 		Comportamiento.Inicializar();
@@ -19,24 +17,5 @@ public class Juego {
 		hola.setSize(100, 100);
 		hola.setVisible(true);
 		System.out.println("Nivel 1 terminado, tu puntaje actual es: "+Mapa.getPuntaje());
-		
-		
-		
-		/*Mapa.getInstancia().setNivelActual(2); //Aumenta el nivel
-		Mapa.getInstancia().reconstruirMapa(); //Reconstruye el mapa
-	
-		Comportamiento.comportamientoNormal(ParaDonde.DERECHA);
-
-		for(int i=0;i<3;i++){
-			Comportamiento.comportamientoNormal(ParaDonde.ARRIBA);
-		}
-
-		for(int i=0;i<14;i++){
-			Comportamiento.comportamientoNormal(ParaDonde.IZQUIERDA);
-		}
-		
-		Comportamiento.comportamientoNormal(ParaDonde.ARRIBA);
-		System.out.println("Nivel 2 terminado, tu puntaje actual es: "+Mapa.getPuntaje());*/
-		
 	}
 }
