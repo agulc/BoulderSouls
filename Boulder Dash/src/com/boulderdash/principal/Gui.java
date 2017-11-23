457315bca3dadff03de49dde466d42562065d312
Nicolas <ghigliazzanicolas@gmail.com>
2017-11-23 02:19:37 -0300

    Menu reglas completo

--- Boulder Dash/src/com/boulderdash/principal/Gui.java
+++ Boulder Dash/src/com/boulderdash/principal/Gui.java
@@ -20,6 +20,7 @@ public class Gui extends JFrame{
 	public static JPanel panelTitulo = new JPanel(new BorderLayout());
 	public static JPanel panelOpciones = new JPanel(new BorderLayout());
 	public static JPanel panelHighScores = new JPanel(new BorderLayout());
+	public static JPanel panelReglas = new JPanel(new BorderLayout());
 	public static JPanel panelJuego;//Contiene a la matriz del juego y su HUD correspondiente
 	public static JPanel panelMatriz;
 	public static JPanel panelHud;
@@ -36,6 +37,7 @@ public class Gui extends JFrame{
 	}
 	
 	
+	
 	private void inicializarGui(){
 
 		JLabel labelTitulo = new JLabel(new ImageIcon("./Texturas/Titulo.png"));
@@ -44,7 +46,7 @@ public class Gui extends JFrame{
 		
 		inicializarOpciones();
 		inicializarHighScores();
-		//inicializarReglas();
+		inicializarReglas();
 		
 		panelTitulo.setBackground(Color.BLACK);
 
@@ -107,9 +109,9 @@ public class Gui extends JFrame{
 		botonReglas.setRolloverIcon(new ImageIcon("./Texturas/Reglas.png"));
 		botonReglas.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				//remove(panelTitulo);
+				remove(panelTitulo);
 				System.out.println("Reglas presionado");
-				//menuReglas();
+				menuReglas();
 			}
 		});
 		
@@ -129,6 +131,127 @@ public class Gui extends JFrame{
 		setVisible(true);
 		
 		
+	}
+	
+
+	private void inicializarReglas()
+	{
+		JLabel label = new JLabel(new ImageIcon("./Texturas/OpcionesWolf.gif"));
+		
+		JButton botonRegresar = new JButton();
+		botonRegresar.setOpaque(false);
+		botonRegresar.setContentAreaFilled(false);
+		botonRegresar.setBorderPainted(false);
+		botonRegresar.setFocusPainted(false);
+		botonRegresar.setIcon(new ImageIcon("./Texturas/BackInerte.png"));
+		botonRegresar.setRolloverIcon(new ImageIcon("./Texturas/Back.png"));
+		botonRegresar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				 remove(panelReglas);
+				 volverDeOpciones();
+				 System.out.println("Regresar presionado");
+			}
+		});
+		
+		Font fuente = new Font("Serif", Font.BOLD, 20);
+
+		JLabel regla1 = new JLabel(new ImageIcon("./Texturas/Rockford.gif"));
+		regla1.setText("Controlas a Rockford");
+		regla1.setFont(fuente);
+		regla1.setForeground(Color.WHITE);
+		
+		JLabel regla2 = new JLabel(new ImageIcon("./Texturas/Diamond.gif"));
+		regla2.setText("Debes juntar un numero minimo de diamantes");
+		regla2.setFont(fuente);
+		regla2.setForeground(Color.WHITE);
+		
+		JLabel regla3 = new JLabel(new ImageIcon("./Texturas/ExitClosed.png"));
+		regla3.setText("Esta es la hoguera");
+		regla3.setFont(fuente);
+		regla3.setForeground(Color.WHITE);
+		
+		JLabel regla4 = new JLabel(new ImageIcon("./Texturas/Exit.gif"));
+		regla4.setText("Cuando juntes los diamantes necesarios, la hoguera se encendera y deberas dirigirte a ella");
+		regla4.setFont(fuente);
+		regla4.setForeground(Color.WHITE);
+		
+		JLabel regla5 = new JLabel(new ImageIcon("./Texturas/boulder.gif"));
+		regla5.setText("Evita ser golpeado por las piedras, aunque puedes empujarlas, y usarlas a tu favor");
+		regla5.setFont(fuente);
+		regla5.setForeground(Color.WHITE);
+		
+		JLabel regla6 = new JLabel(new ImageIcon("./Texturas/dirt.gif"));
+		regla6.setText("Puedes moverte libremente a traves de la tierra");
+		regla6.setFont(fuente);
+		regla6.setForeground(Color.WHITE);
+		
+		JLabel regla7 = new JLabel(new ImageIcon("./Texturas/firefly.gif"));
+		regla7.setText("Las luciernagas explotan cuando las tienes cerca");
+		regla7.setFont(fuente);
+		regla7.setForeground(Color.WHITE);
+		
+		JLabel regla8 = new JLabel(new ImageIcon("./Texturas/butterfly.gif"));
+		regla8.setText("Las mariposas tambien explotan, pero puedes matarlas con piedras para conseguir diamantes");
+		regla8.setFont(fuente);
+		regla8.setForeground(Color.WHITE);
+		
+		JLabel regla9 = new JLabel(new ImageIcon("./Texturas/magic.gif"));
+		regla9.setText("Puedes romper las paredes claras con explosiones");
+		regla9.setFont(fuente);
+		regla9.setForeground(Color.WHITE);
+		
+		JLabel regla10 = new JLabel(new ImageIcon("./Texturas/steel.gif"));
+		regla10.setText("Las paredes oscuras son indestructibles");
+		regla10.setFont(fuente);
+		regla10.setForeground(Color.WHITE);
+		
+		JLabel regla11 = new JLabel(new ImageIcon("./Texturas/heart.png"));
+		regla11.setText("Tienes 4 vidas por nivel");
+		regla11.setFont(fuente);
+		regla11.setForeground(Color.WHITE);
+		
+		JLabel regla12 = new JLabel(new ImageIcon("./Texturas/clock.gif"));
+		regla12.setText("Hay un limite de tiempo para completar los niveles");
+		regla12.setFont(fuente);
+		regla12.setForeground(Color.WHITE);
+		
+		JLabel regla13 = new JLabel(new ImageIcon("./Texturas/Amoeba.gif"));
+		regla13.setText("Las amebas estan programadas pero no aparecen en ningun nivel ( ͡° ͜ʖ ͡°)");
+		regla13.setFont(fuente);
+		regla13.setForeground(Color.WHITE);
+		
+		
+		
+		
+		Container containerReglas = new Container();
+		Container container = new Container();
+		Container container2 = new Container();
+		
+		containerReglas.setLayout(new GridLayout(0,1));
+		containerReglas.add(regla1);
+		containerReglas.add(regla2);
+		containerReglas.add(regla3);
+		containerReglas.add(regla4);
+		containerReglas.add(regla5);
+		containerReglas.add(regla6);
+		containerReglas.add(regla7);
+		containerReglas.add(regla8);
+		containerReglas.add(regla9);
+		containerReglas.add(regla10);
+		containerReglas.add(regla11);
+		containerReglas.add(regla12);
+		containerReglas.add(regla13);
+		
+		container.setLayout(new BorderLayout()); //Donde va la lista de reglas
+		container.add(label, BorderLayout.NORTH);
+		container.add(containerReglas, BorderLayout.SOUTH);
+		
+		container2.setLayout(new BorderLayout());
+		
+		container2.add(botonRegresar, BorderLayout.SOUTH);
+		panelReglas.setBackground(Color.BLACK);
+		panelReglas.add(container);
+		panelReglas.add(container2, BorderLayout.SOUTH);
 	}
 	
 	private void inicializarHighScores()
@@ -253,6 +376,14 @@ public class Gui extends JFrame{
 	{
 		this.add(panelOpciones);
 		this.setSize(panelTitulo.getSize());
+		repaint();
+	}
+	
+	private void menuReglas()
+	{
+		this.add(panelReglas);
+		this.setSize( (int)panelTitulo.getSize().getWidth(), 630);
+		
 		repaint();
 	}
 	
