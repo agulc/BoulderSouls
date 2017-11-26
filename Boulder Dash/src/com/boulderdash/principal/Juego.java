9e02999b150ca89f8dc7d0bb50b966addf11a022
unknown <fedeblasco@hotmail.com>
2017-11-26 00:35:11 -0300

    Esto no anda

--- Boulder Dash/src/com/boulderdash/principal/Juego.java
+++ Boulder Dash/src/com/boulderdash/principal/Juego.java
@@ -1,6 +1,11 @@
 package com.boulderdash.principal;
 
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import com.boulderdash.audio.Audio;
+import com.boulderdash.entradasalida.Highscore;
 import com.boulderdash.entradasalida.OpcionesES;
 import com.boulderdash.interfaz.Gui;
 
@@ -16,6 +21,8 @@ public class Juego{
 		Gui.getInstancia();
 		Audio.titulo();
 		Audio.musicaMenu();
+		Highscore high = new Highscore("",0,0);
+		Gui.getInstancia().getNuevoHighscore().menuNuevoHighscore(high);
 
 	}	
 }
